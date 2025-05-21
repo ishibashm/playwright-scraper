a47a4a0d1076f0ce511ab142da43ce4a01d3fef4
ishibashm <ishibashm@gmail.com>
2025-05-21 10:43:35 +0900

    リーダーの閉じ方をオプショナルチェーンを使用して簡潔に修正。

--- src/index.ts
+++ src/index.ts
@@ -65,7 +65,7 @@ async function main() {
     }
     rl = readline.createInterface({ input, output });
     return new Promise<string>(resolve => rl.question(query, resolve)).finally(() => {
-      rl && rl.close();
+      rl?.close();
     });
   };
 
